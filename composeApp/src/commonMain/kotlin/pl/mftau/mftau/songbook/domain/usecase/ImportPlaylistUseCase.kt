df0bb9563ba2e053c75cb34c99de591f0e128f64
akobartek <sokolowskijbartek@gmail.com>
2024-12-03 00:33:06 +0100

    import playlist error

--- composeApp/src/commonMain/kotlin/pl/mftau/mftau/songbook/domain/usecase/ImportPlaylistUseCase.kt
+++ composeApp/src/commonMain/kotlin/pl/mftau/mftau/songbook/domain/usecase/ImportPlaylistUseCase.kt
@@ -1,6 +1,7 @@
 package pl.mftau.mftau.songbook.domain.usecase
 
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import pl.mftau.mftau.songbook.domain.model.Playlist
 import pl.mftau.mftau.songbook.domain.repository.RemotePlaylistRepository
 import pl.mftau.mftau.songbook.domain.repository.TextsSongBookRepository
@@ -9,15 +10,24 @@ class ImportPlaylistUseCase(
     private val textsSongBookRepository: TextsSongBookRepository,
     private val remotePlaylistRepository: RemotePlaylistRepository
 ) {
-    suspend fun getPlaylist(importCode: String) =
-        if (importCode.startsWith("fs:"))
-            remotePlaylistRepository.getPlaylist(importCode.split(":")[1])
-        else flow {
+    suspend fun getPlaylist(importCode: String) = when {
+        importCode.split(":").getOrNull(1) == null ->
+            flowOf(null)
+
+        importCode.startsWith("fs:") ->
+            importCode.split(":")[1].let { id ->
+                remotePlaylistRepository.getPlaylist(id)
+            }
+
+        else -> flow {
             val songs = textsSongBookRepository.getSongs().toMutableList()
             val playlistSongs =
-                importCode.split(":")[1].split("~").map { songNumber ->
-                    songs[songNumber.toInt() - 1]
-                }
+                importCode.split(":")[1].split("~")
+                    .filter { it.toIntOrNull() != null }
+                    .map { songNumber ->
+                        songs[songNumber.toInt() - 1]
+                    }
             emit(Playlist(songs = playlistSongs))
         }
+    }
 }
