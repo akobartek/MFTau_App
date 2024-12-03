8b9fa203c1baae46b549dd2d9448de1d1b3e8ccf
akobartek <sokolowskijbartek@gmail.com>
2024-12-04 00:31:37 +0100

    pdf files on ios

--- composeApp/src/nativeMain/kotlin/pl/mftau/mftau/common/presentation/ApplePdfOpener.kt
+++ composeApp/src/nativeMain/kotlin/pl/mftau/mftau/common/presentation/ApplePdfOpener.kt
@@ -1,10 +1,38 @@
 package pl.mftau.mftau.common.presentation
 
-class ApplePdfOpener: PdfOpener {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
+import platform.UIKit.UIDocumentInteractionController
+import platform.UIKit.UIDocumentInteractionControllerDelegateProtocol
+import platform.UIKit.UIViewController
+import platform.darwin.NSObject
+
+class ApplePdfOpener : PdfOpener {
+
+    private val pdfOpener = MyPdfOpener()
 
     override suspend fun openPdf(
         fileName: String,
         uri: String,
         bytes: ByteArray,
-    ): Boolean = true
+    ): Boolean = pdfOpener.openPdf(uri)
+}
+
+private class MyPdfOpener : NSObject(), UIDocumentInteractionControllerDelegateProtocol {
+
+    suspend fun openPdf(uri: String): Boolean = withContext(Dispatchers.Main) {
+        UIApplication.sharedApplication.keyWindow?.rootViewController?.let {
+            val documentInteractionController =
+                UIDocumentInteractionController.interactionControllerWithURL(NSURL(string = uri))
+            documentInteractionController.delegate = this@MyPdfOpener
+            documentInteractionController.presentPreviewAnimated(true)
+            true
+        } ?: false
+    }
+
+    override fun documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController): UIViewController {
+        return UIApplication.sharedApplication.keyWindow!!.rootViewController!!
+    }
 }
